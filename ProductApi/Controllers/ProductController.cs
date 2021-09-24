028f90ef4e9cbc0225e884d093552d7ce5df9025
alprsln <alpars44@hotmail.com>
2021-09-24 18:37:30 +0300

    feat!: add controller, model and dto for...
    ...product

---
+++ ProductApi/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductApi.Dtos;
+using ProductApi.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductApi.Controllers
+{
+    [ApiController]
+    [Route("api/v1/products")]
+    public class ProductController : ControllerBase
+    {
+
+        [HttpGet]
+        public ActionResult<ProductDto> GetProducts()
+        {
+
+            List<ProductDto> list = new List<ProductDto>();
+            list.Add(new ProductDto
+            {
+                Id = 1,
+                Name = "Chair",
+                Description = "asd",
+                Price = 50
+            });
+            return Ok(list);
+        }
+    }
+}
---
+++ ProductApi/Dtos/CreateProductDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductApi.Dtos
+{
+    public class CreateProductDto
+    {
+        //TODO: Add validation
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+    }
+}
