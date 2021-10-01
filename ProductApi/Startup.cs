e1c209584b317fe763b98e2928005001cb3d7ab4
alprsln <alpars44@hotmail.com>
2021-10-01 21:14:24 +0300

    docs!: implement swagger and link readme

--- ProductApi/Startup.cs
+++ ProductApi/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using ProductApi.Models;
 using ProductApi.Services;
 using System;
@@ -34,6 +35,17 @@ namespace ProductApi
 
             services.AddDbContext<ProductContext>(opt =>
                                                opt.UseInMemoryDatabase("Products"));
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                { 
+                    Version = "v1",
+                    Title = "Product API",
+                    Description = "A simple API that is responsible for delivering" +
+                        "product information for the e-commerce website Hepsinerede."
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,6 +55,14 @@ namespace ProductApi
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Product API v1");
+                c.RoutePrefix = string.Empty;
+            });
 
             app.UseRouting();
 
