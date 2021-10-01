63de828cde8e347bc45355c0b44bae95ad5ac8cf
alprsln <alpars44@hotmail.com>
2021-10-01 19:12:33 +0300

    feat: add generic sort extension

---
+++ ProductApi/Services/Extensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductApi.Services
+{
+    public static class Extensions
+    {
+        public static List<T> SortBy<T>(this List<T> source, string sortParameter)
+        {
+
+            if (source == null || sortParameter == null)
+                throw new ArgumentNullException();
+
+            sortParameter = char.ToUpper(sortParameter[0]) + sortParameter[1..].ToLower();
+            //source = source.OrderBy(s => s. sortParameter);
+            source = (from item in source
+                     orderby sortParameter
+                     select item).ToList();
+
+            return source;
+        }
+    }
+}
